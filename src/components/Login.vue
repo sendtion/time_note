642d6fbd76319da16a715af03d74bd4a0240a6a3
sendtion <sendtion@163.com>
2019-12-17 18:32:49 +0800

    user element-ui

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,20 +1,26 @@
 <template>
-  <el-container>
-    <el-main>
-      <div class="login">
-        <p><input type="text" placeholder="输入用户名" v-model="username" /></p>
-        <p><input type="text" placeholder="输入密码" v-model="password" /></p>
-        <p><input type="button" value="登录" @click="login" /></p>
-        <p><input type="button" value="退出登录" @click="logout" /></p>
-      </div>
-      <el-input v-model="username" placeholder="输入用户名" clearable maxlength="10" show-word-limit></el-input>
-      <div style="margin: 20px 0;"></div>
-      <el-input v-model="password" placeholder="输入密码" clearable show-password></el-input>
-      <div style="margin: 20px 0;"></div>
-      <el-button type="primary" round @click="login">登录</el-button>
-      <el-button type="success" round @click="logout">退出登录</el-button>
-    </el-main>
-  </el-container>
+  <div class="note-list">
+    <el-container>
+      <el-main>
+        <h2>时光笔记</h2>
+        <div style="margin: 50px 0;"></div>
+        <el-input v-model="username" placeholder="输入用户名" clearable maxlength="10" show-word-limit></el-input>
+        <div style="margin: 30px 0;"></div>
+        <el-input v-model="password" placeholder="输入密码" clearable show-password></el-input>
+        <div style="margin: 50px 0;"></div>
+        <el-button type="primary" round @click="login">登录</el-button>
+        <el-button type="success" round @click="logout">退出登录</el-button>
+      </el-main>
+      <!-- 对话框提示 -->
+      <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" center="true" show-close="false">
+        <span>请输入用户名或者密码</span>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        </span>
+      </el-dialog>
+    </el-container>
+  </div>
 </template>
 
 <script>
@@ -23,28 +29,29 @@ export default {
   data() {
     return {
       username:'',
-      password:''
+      password:'',
+      dialogVisible: false
     }
   },
   methods: {
     login() {
-      console.log("登录了")
       if(this.username != '' && this.password != ''){
         Bmob.User.login(this.username,this.password).then(res => {
-          alert('登录成功：', res.username, res.sex)
+          //alert('登录成功：', res.username, res.sex)
           console.log(res, res.objectId)
           // 和push的区别，push有记录一个history，replace没有
           // this.$router.push({name: "/detail", params: {id: e}})
           // this.$router.push({path: "/detail", query: {id: e}})
+          // 参数获取，params和query区别，query参数在地址栏显示，params的参数不在地址栏显示
+          // id = this.$route.params.id
           // id = this.$route.query.id
-          this.$router.replace({name: 'NoteList', params: {id: res.objectId}})
-          //参数获取，params和query区别，query参数在地址栏显示，params的参数不在地址栏显示
-          //id = this.$route.params.id
+          this.$router.replace({path: '/note_list'})
         }).catch(err => {
           console.log(err)
         });
       } else {
-        alert('请输入用户名或者密码')
+        //alert('请输入用户名或者密码')
+        this.dialogVisible = true;
       }
     },
     logout(){
@@ -56,5 +63,7 @@ export default {
 </script>
 
 <style>
-  
+  .el-input {
+    width: 30%;
+  }
 </style>
--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,15 +1,60 @@
 <template>
-    <div class="note-list">
-        <h1>笔记列表</h1>
-        <input type="button" value="添加笔记" @click="addNote">
-        <ul v-for="item in notes" :key="item.objectId">
-            <li>
-                <!-- to不加冒号不能正常跳转 -->
-                <router-link :to="{path:'/note_detail', query:{id:item.objectId}}" target='_blank'>
-                    [{{item.group.groupName}}] {{item.title}}
-                </router-link>
-            </li>
-        </ul>
+    <div class="note-list" align="left">
+        <!-- <h2>笔记列表</h2> -->
+        <!-- <input type="button" value="添加笔记" @click="addNote"> -->
+        <el-button type="primary" round @click="addNote">
+            添加笔记
+        </el-button>
+        
+        <div style="margin: 30px 0;"></div>
+
+        <el-table
+            :data="notes"
+            style="width: 100%;"
+            :default-sort = "{prop: 'createdAt', order: 'descending'}"
+            @row-click="toNoteDetail">
+            <el-table-column
+                prop="createdAt"
+                sortable
+                label="日期"
+                width="200">
+                <template slot-scope="scope">
+                    <i class="el-icon-time"></i>
+                    <span style="margin-left: 10px">{{ scope.row.createdAt }}</span>
+                </template>
+            </el-table-column>
+
+            <el-table-column
+                label="分类"
+                width="200">
+                <template slot-scope="scope">
+                    <span>{{ scope.row.group.groupName }}</span>
+                </template>
+            </el-table-column>
+
+            <el-table-column
+                label="标题">
+                <template slot-scope="scope">
+                    <!-- <span>{{ scope.row.title }}</span> -->
+                    <!-- to不加冒号不能正常跳转 -->
+                    <router-link :to="{path:'/note_detail', query:{id:scope.row.objectId}}" target='_blank'>
+                        {{scope.row.title}}
+                    </router-link>
+                </template>
+            </el-table-column>
+
+            <el-table-column label="操作" fixed="right">
+                <template slot-scope="scope">
+                    <el-button
+                    size="mini"
+                    @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                    <el-button
+                    size="mini"
+                    type="danger"
+                    @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
@@ -18,6 +63,7 @@ export default {
     name: 'NoteList',
     data () {
         return {
+            reverse: true,
             notes: []
         }
     },
@@ -34,7 +80,15 @@ export default {
             });
         },
         addNote(){
-            this.$router.push({name: 'AddNote'});
+            //this.$router.push({path: '/add_note'});
+            const {href} = this.$router.resolve({path: '/add_note'})
+            window.open(href,'_blank')
+        },
+        toNoteDetail(row,event,column){
+            //console.log(row.objectId, event, column);
+            //this.$router.push({path: '/note_detail', query: {id: row.objectId}});
+            //const {href} = this.$router.resolve({path: '/note_detail', query: {id: row.objectId}})
+            //window.open(href,'_blank')
         }
     }
 }
